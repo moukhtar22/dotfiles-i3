dcd5a34a7372c9152adc5edc728d48ddd42f148c
dastarruer <ayushpramanik399@gmail.com>
2024-12-05 21:39:38 -0400

    Added wallpapers

--- bin/bin/commit_dotfiles.sh
+++ bin/bin/commit_dotfiles.sh
@@ -4,18 +4,6 @@ CURRENT_DIR=$(pwd)
 
 cd ~/dotfiles
 
-git add .
-git commit -m "Automated commit"
-git push
-
-cd "$CURRENT_DIR"
-echo "Finished commit"
-#!/bin/bash
-
-CURRENT_DIR=$(pwd)
-
-cd ~/dotfiles
-
 # Check if a commit message argument is provided
 if [ -z "$1" ]; then
   echo "Commit message pls"
