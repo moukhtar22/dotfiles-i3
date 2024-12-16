b45b7ab5fe7dc0cf8ea36dee4f3163131c43bba7
dastarruer <ayushpramanik399@gmail.com>
2024-12-15 21:06:33 -0400

    changed polybar launch.sh

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Check if Polybar is running
-if pgrep -x "polybar" > /dev/null
-then
-    # If Polybar is running, kill it
-    killall polybar
-    echo "Polybar stopped"
-else
-    # If Polybar is not running, start it
-    $HOME/.config/polybar/launch.sh
-    echo "Polybar started"
-fi
+pkill polybar
+echo "Polybar stopped"
+
+polybar right
+polybar center
+polybar left
+echo "Polybar started"
+
