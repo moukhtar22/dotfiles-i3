26684cb8c1e7bf6f756db092ad8538caed71a289
dastarruer <ayushpramanik399@gmail.com>
2024-12-15 22:12:55 -0400

    polybar fixes, shortcut for toggle picom

--- bin/bin/toggle_picom.sh
+++ bin/bin/toggle_picom.sh
@@ -1,32 +1,10 @@
 #!/bin/bash
 
-# Paths to the Picom configuration files
-CONFIG_NO_BLUR="$HOME/.config/picom/picom_no_blur.conf"
-CONFIG_BLUR="$HOME/.config/picom/picom.conf"
-
-# Check if Picom is running
-if pgrep -x "picom" > /dev/null; then
-    # Get the current config file Picom is using
-    CURRENT_CONFIG=$(ps aux | grep "[p]icom" | grep -- '--config' | awk '{for (i=1;i<=NF;i++) if ($i=="--config") print $(i+1)}')
-
-    # Resolve tilde to home directory
-    RESOLVED_CONFIG_NO_BLUR=$(eval echo "$CONFIG_NO_BLUR")
-    RESOLVED_CONFIG_BLUR=$(eval echo "$CONFIG_BLUR")
-
-    # Determine which config is being used and switch to the other
-    if [[ "$CURRENT_CONFIG" == "$RESOLVED_CONFIG_NO_BLUR" ]]; then
-        # Switch to blur config
-        pkill picom
-        picom --config "$RESOLVED_CONFIG_BLUR" &
-        echo "Switched to Picom with blur config."
-    else
-        # Switch to no blur config
-        pkill picom
-        picom --config "$RESOLVED_CONFIG_NO_BLUR" &
-        echo "Switched to Picom with no blur config."
-    fi
+if pgrep -x "picom" > /dev/null
+then
+    echo "Picom is running. Stopping Picom..."
+    pkill picom
 else
-    # If Picom is not running, start it with the default config
-    picom --config "$CONFIG_BLUR" &
-    echo "Started Picom with default blur config."
+    echo "Picom is not running. Starting Picom..."
+    picom --config ~/.config/picom/picom.conf &
 fi
