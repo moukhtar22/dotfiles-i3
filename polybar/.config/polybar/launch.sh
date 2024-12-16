f0da61c6fa847cf1e4711f5ff0cea52f5e6e4ccc
dastarruer <ayushpramanik399@gmail.com>
2024-12-15 21:58:07 -0400

    only one polybar because i3 hates multiple polybars

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-pkill polybar
-echo "Polybar stopped"
+# Terminate already running bar instances
+killall -q polybar
 
-polybar right
-polybar center
-polybar left
-echo "Polybar started"
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar main &
+# polybar center &
+# polybar left &
+
+
+echo "Polybar launched..."
