f96d7660974bcbae40d2c41097eb6ff3e22e9e24
dastarruer <ayushpramanik399@gmail.com>
2024-12-16 18:52:42 -0400

    i3 config stuff

--- bin/bin/change_wallpaper.sh
+++ bin/bin/change_wallpaper.sh
@@ -3,8 +3,8 @@
 # Directory containing wallpapers
 WALLPAPER_DIR="$HOME/dotfiles/Wallpapers/Pictures/Wallpapers"
 
-# Get the current wallpaper
-CURRENT_WALLPAPER=$(xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitoreDP/workspace0/last-image)
+# Get the current wallpaper (feh stores this in an X property)
+CURRENT_WALLPAPER=$(xprop -root _XROOTPMAP_ID | awk '{print $3}')
 
 # Get a random wallpaper from the directory
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
@@ -15,7 +15,7 @@ while [[ "$WALLPAPER" == "$CURRENT_WALLPAPER" ]]; do
     WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
 done
 
-# Set the new wallpaper
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitoreDP/workspace0/last-image -s "$WALLPAPER"
+# Set the new wallpaper using feh
+feh --bg-scale "$WALLPAPER"
 
 echo "Wallpaper set to $WALLPAPER"
