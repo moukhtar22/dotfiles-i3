edde6cb0c353138296f283c8fdc0a917a407de61
dastarruer <ayushpramanik399@gmail.com>
2025-01-07 16:57:14 -0400

    stuff

--- bin/bin/window_event_listener.sh
+++ bin/bin/window_event_listener.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+if [ -z "$DISPLAY" ]; then
+    export DISPLAY=:0
+fi
+
 # Path to your save_workspaces script
-SAVE_WORKSPACES_SCRIPT="~/bin/save_workspace_layouts.sh"
+SAVE_WORKSPACES_SCRIPT="save_workspace_layouts.sh"
 
 # Listen for window events and execute the save script on relevant changes
-i3-msg -t subscribe '[ "window" ]' | while read -r event; do
-    if echo "$event" | grep -q '"change":"new"'; then
-        echo "New window detected. Saving workspaces..."
-        bash "$SAVE_WORKSPACES_SCRIPT"
-    elif echo "$event" | grep -q '"change":"close"'; then
-        echo "Window closed. Saving workspaces..."
-        bash "$SAVE_WORKSPACES_SCRIPT"
-    fi
+while true; do
+    i3-msg -t subscribe '[ "window" ]' | while read -r event; do
+        if echo "$event" | grep -q '"change":"new"'; then
+            echo "New window detected. Saving workspaces..."
+            "$SAVE_WORKSPACES_SCRIPT"
+        elif echo "$event" | grep -q '"change":"close"'; then
+            echo "Window closed. Saving workspaces..."
+            bash "$SAVE_WORKSPACES_SCRIPT"
+        fi
+    done
 done
